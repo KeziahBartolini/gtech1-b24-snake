b94f1e2e7849b9c716a5450a1559c9a38e5fd00b
KeziahBartolini <92040185+KeziahBartolini@users.noreply.github.com>
2022-02-06 17:20:05 +0100

    Update fruit.cpp
    fruit.cpp complet

--- clement/fruit.cpp
+++ clement/fruit.cpp
@@ -8,11 +8,22 @@
 #define RAND
 
 fruit::fruit(int x, int y){
-    this->x;
-    this->y;
+ apple();
 }
 
-void apple(){
-    x = (rand() % ( - MIN + 1)) + MIN;
+void fruit::apple(){
+   this->x = (rand() % WIDTH/20);
+   this->y = (rand() % HEIGHT/20);
+      
 }
 
+void fruit::draw(SDL_Renderer*renderer){
+    SDL_Rect fruit;
+    fruit.x =  this->x*30;
+    fruit.y =  this->y*30;
+    fruit.w = 30;
+    fruit.h = 30;
+    SDL_SetRenderDrawColor(renderer,255,255,255,SDL_ALPHA_OPAQUE);
+    SDL_RenderDrawRect(renderer,&fruit);
+    SDL_RenderPresent(renderer);
+}
